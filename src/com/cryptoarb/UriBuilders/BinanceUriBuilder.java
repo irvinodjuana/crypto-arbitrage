7f916ff3acc3916f6d4a268dcfc63fd0f17a51a0
Irvino Djuana <irvino.djuana@gmail.com>
2021-01-04 12:48:49 -0800

    Feature - add config provider to BinanceUriBuilder

--- src/com/cryptoarb/UriBuilders/BinanceUriBuilder.java
+++ src/com/cryptoarb/UriBuilders/BinanceUriBuilder.java
@@ -1,12 +1,15 @@
 package com.cryptoarb.UriBuilders;
 
+import com.cryptoarb.Configuration.IConfigurationProvider;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class BinanceUriBuilder implements IBinanceUriBuilder {
-    private static final String BookTickerStreamUriBase = "wss://stream.binance.com:9443/stream?streams=";
+    private String multiStreamUriBase = "wss://stream.binance.com:9443/stream?streams=";
 
-    public BinanceUriBuilder() {
+    public BinanceUriBuilder(IConfigurationProvider configurationProvider) {
+        multiStreamUriBase = configurationProvider.getBinanceStreamUri() + "/streams?streams=";
     }
 
     public String buildBookTickerStreamUri(List<String> symbols) {
@@ -14,7 +17,7 @@ public class BinanceUriBuilder implements IBinanceUriBuilder {
                 .map(BinanceUriBuilder::getStreamName)
                 .collect(Collectors.joining("/"));
 
-        return BookTickerStreamUriBase + tickerNames;
+        return multiStreamUriBase + tickerNames;
     }
 
     private static String getStreamName(String symbol) {
